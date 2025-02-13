c142b50ed551511eb64c9ff355882f4b04308bf8
amirseade <amirseade0@gmail.com>
2025-02-13 15:06:10 -0300

    FUNCIONANDO PORTAFOLIO

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();  // Prevent the default form submission
+// Seleccionamos todos los enlaces dentro de la navegación
+const enlaces = document.querySelectorAll('.navegacion a');
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+// Agregamos el evento de clic a cada enlace
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', function () {
+        // Eliminar la clase "activo" de todos los enlaces
+        enlaces.forEach(link => link.classList.remove('activo'));
 
-    if (name && email && message) {
-        alert('Formulario enviado con éxito!');
-        document.getElementById('contact-form').reset();
-    } else {
-        alert('Por favor, completa todos los campos.');
-    }
+        // Agregar la clase "activo" al enlace que se ha clickeado
+        this.classList.add('activo');
+    });
 });
+
+
+document.getElementById('formularioContacto').addEventListener('submit', function (event) {
+    event.preventDefault();  // Evita el envío tradicional del formulario
+
+    // Capturamos los datos del formulario
+    const nombre = document.querySelector('[name="nombre"]').value;
+    const telefono = document.querySelector('[name="telefono"]').value;
+    const correo = document.querySelector('[name="correo"]').value;
+    const mensaje = document.querySelector('[name="mensaje"]').value;
+
+    // Verificamos si todos los campos están llenos
+    if (!nombre || !telefono || !correo || !mensaje) {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
+
+    // Creamos el mensaje que se enviará por WhatsApp
+    const mensajeWhatsApp = `*Necesito mas información sobre sus servicios.*\n\n*Nombre:* ${nombre}\n*Teléfono:* ${telefono}\n*Correo:* ${correo}\n*Mensaje:* ${mensaje}`;
+    console.log(mensajeWhatsApp);
+
+    // Codificamos el mensaje para usarlo en una URL
+    const mensajeEncoded = encodeURIComponent(mensajeWhatsApp);
+
+    // Creamos la URL de WhatsApp
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=3855277213&text=${mensajeEncoded}&type=phone_number&app_absent=0`;
+
+    // Redirigimos al WhatsApp directamente
+    window.location.href = whatsappUrl; 
+});
+
